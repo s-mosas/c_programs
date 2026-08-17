4f06f8fe917c7f42070365aa3d801edef4d495f9
s-mosas <s.mosas.be.engg@gmail.com>
2026-08-17 22:06:34 +0530

    positive_negative_zero.c
    C program to determine whether a user-entered number is positive, negative, or zero.

---
+++ positive_negative_zero.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+printf("Enter your number:");
+int a;
+scanf("%d",&a);
+
+if(a>0) {
+printf("%d is positive number\n",a); }
+else if (a == 0) {
+printf("It is zero\n"); }   give name and discription for github 
+else{
+printf("%d is negative number\n",a); }
+
+return 0;
