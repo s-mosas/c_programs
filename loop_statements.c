b1eca896fc11d3194e97f5747eb56b1c9f0fa250
s-mosas <s.mosas.be.engg@gmail.com>
2026-09-15 19:52:19 +0530

    Add loop statements example in loop_statements.c
    Implement for, while, and do-while loops to print numbers.

---
+++ loop_statements.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+printf("For loop output\n");
+for(int i=1;i <= 10;i++){
+printf("%d\n",i);}
+
+printf("While loop outputs\n");
+int a=1;
+while(a <= 10){
+printf("%d\n",a);
+a++:}
+
+printf("Do-While loop output\n");
+int b=1;
+do {
+printf("%d\n",b);
+b++:
+}while(b <= 5);
+
+return 0;
+}
